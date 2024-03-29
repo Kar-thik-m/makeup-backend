23465bd81abb9f96368e6c1a29c21355d072f73f
kar-thik-m <sparrowkarthik007@gmail.com>
2024-03-29 15:44:14 +0530

    cors

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from "express";
 import connectToDb from "./Mongodb/connection.js";
 import itemRouter from "./Route/ItemRouter.js";
 import userRouter from "./Route/user.js";
+import cors from "cors";
 const app = express()
 
 await connectToDb();
 app.use(express.json());
-
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 app.use("/product", itemRouter);
 app.use("/user",userRouter);
