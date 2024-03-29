09a0a94a558106eac33676559d34f8e60d4c60f1
kar-thik-m <sparrowkarthik007@gmail.com>
2024-03-29 14:09:04 +0530

    iyi

--- Mongodb/connection.js
+++ Mongodb/connection.js
@@ -17,11 +17,7 @@ const cloudMongoUrl=`mongodb+srv://${username}:${password}@${clusterName}/${dbNa
 const connectToDb = async () => {
     try {
         await mongoose.connect(cloudMongoUrl,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-
-            });
+          );
         console.log("db Connecter successfully");
     } catch (err) {
         console.log(err);
